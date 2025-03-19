b04eaf59571a5cb10e146a86e73f94af169913c9
DESKTOP-LKZ\laure <laurenskz@gmail.com>
2025-03-19 12:22:24 +0100

    Make dirs for protos in docker

--- protos.sh
+++ protos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p ./com/baboea/models ./com/baboea/services
 python3 -m grpc_tools.protoc -I./protos/foodopt --python_out=./ --pyi_out=./ --grpc_python_out=./ ./protos/foodopt/com/baboea/models/*.proto
 python3 -m grpc_tools.protoc -I./protos/foodopt --python_out=./ --pyi_out=./ --grpc_python_out=./ ./protos/foodopt/com/baboea/*.proto
 python3 -m grpc_tools.protoc -I./protos/foodopt --python_out=./ --pyi_out=./ --grpc_python_out=./ ./protos/foodopt/com/baboea/services/*.proto
